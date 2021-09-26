374b1b8f4020ac190c085bc7f3b71694c7e16dcf
DImuthu Weerathunga <dimu.wa.98@gmail.com>
2021-09-26 12:54:46 +0530

    fefe

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const sectreNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = sectreNumber;
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  } else if (guess === sectreNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  }
+});
